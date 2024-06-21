85bde6602d7e1689161d29c67dd590276f1d7172
imarwanmokhtar <marwanmokhtar000@gmail.com>
2024-06-21 22:18:36 +0200

    fix errors

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -64,9 +64,9 @@ export async function POST(req: Request) {
         const user = {
             clerkId: id,
             email: email_addresses[0].email_address,
-            username: username!,
-            firstName: first_name,
-            lastName: last_name,
+            username: username ?? '',
+            firstName: first_name ?? '',
+            lastName: last_name ?? '',
             photo: image_url,
         };
 
@@ -89,9 +89,9 @@ export async function POST(req: Request) {
         const { id, image_url, first_name, last_name, username } = evt.data;
 
         const user = {
-            firstName: first_name,
-            lastName: last_name,
-            username: username!,
+            firstName: first_name ?? '',
+            lastName: last_name ?? '',
+            username: username ?? '',
             photo: image_url,
         };
 
@@ -104,7 +104,13 @@ export async function POST(req: Request) {
     if (eventType === "user.deleted") {
         const { id } = evt.data;
 
-        const deletedUser = await deleteUser(id!);
+        if (!id) {
+            return new Response("Error occurred -- no ID provided", {
+                status: 400,
+            });
+        }
+
+        const deletedUser = await deleteUser(id.toString());
 
         return NextResponse.json({ message: "OK", user: deletedUser });
     }
