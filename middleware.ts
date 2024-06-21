7e2a1a2593488e69e7ccc39c0212528a59bcc4e8
imarwanmokhtar <marwanmokhtar000@gmail.com>
2024-06-21 22:08:17 +0200

    implement Clerk Webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
-import { authMiddleware } from "@clerk/nextjs/server";
-
-export default authMiddleware();
-
+import { authMiddleware } from "@clerk/nextjs/server"; 
+export default authMiddleware({
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
+});
+ 
 export const config = {
-    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
